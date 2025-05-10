8f890b29313759373de3bb8ac0df16100d68914a
renatodellosso <rddellosso4@gmail.com>
2025-05-09 22:12:25 -0400

    Basic bytecode is done

--- src/chunk.c
+++ src/chunk.c
@@ -8,23 +8,36 @@ void initChunk(Chunk* chunk)
 	chunk->count = 0;
 	chunk->capacity = 0;
 	chunk->code = NULL;
+	chunk->lines = NULL;
+	initValueArray(&chunk->constants); // constants isn't a pointer, so we need & to get the address
 }
 
 void freeChunk(Chunk* chunk)
 {
 	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+	FREE_ARRAY(int, chunk->lines, chunk->capacity);
+	freeValueArray(&chunk->constants);
 	initChunk(chunk); // Zero-out the chunk to ensure the state is defined
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte)
+// Use uint8_t (8-bit unsigned) - it's smaller
+void writeChunk(Chunk* chunk, uint8_t byte, int line)
 {
 	if (chunk->capacity < chunk->count + 1)
 	{
 		int oldCapacity = chunk->capacity;
 		chunk->capacity = GROW_CAPACITY(oldCapacity);
 		chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+		chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
 	}
 
 	chunk->code[chunk->count] = byte;
+	chunk->lines[chunk->count] = line;
 	chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value)
+{
+	writeValueArray(&chunk->constants, value);
+	return chunk->constants.count - 1; // Use '.' since constants is not a pointer
 }
---
+++ src/debug.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#include "debug.h"
+#include "value.h"
+
+void disassembleChunk(Chunk* chunk, const char* name)
+{
+	printf("== %s ==\n", name);
+
+	for (int offset = 0; offset < chunk->count;)
+	{
+		offset = disassembleInstruction(chunk, offset);
+	}
+}
+
+int disassembleInstruction(Chunk* chunk, int offset)
+{
+	// %04d makes it 4 characters wide
+	printf("%04d ", offset);
+
+	// Print line #
+	if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
+	{
+		printf("	| ");
+	}
+	else
+	{
+		printf("%4d ", chunk->lines[offset]);
+	}
+
+	// Print instruction
+	uint8_t	instruction = chunk->code[offset];
+	switch (instruction)
+	{
+	case OP_CONSTANT:
+		return constantInstruction("OP_CONSTANT", chunk, offset);
+	case OP_RETURN:
+		return simpleInstruction("OP_RETURN", offset);
+	default:
+		printf("Unknown opcod %d\n", instruction);
+		return offset + 1;
+	}
+}
+
+// static funcs can only be accessed in the file they are defined in
+static int simpleInstruction(const char* name, int offset)
+{
+	printf("%s\n", name);
+	return offset + 1;
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset)
+{
+	uint8_t	constant = chunk->code[offset + 1]; // Value is next in bytecode
+
+	printf("%-16s %4d '", name, constant); // -16s is a 16-char, left-aligned string
+	printValue(chunk->constants.values[constant]);
+	printf("'\n");
+
+	return offset + 2;
+}
