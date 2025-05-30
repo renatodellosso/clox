ae11d1c7363498bddb5f667e4ea06cb286db1491
renatodellosso <rddellosso4@gmail.com>
2025-05-30 12:19:46 -0400

    Started custom reallocate

--- src/memory.c
+++ src/memory.c
@@ -1,7 +1,72 @@
 #include <stdlib.h>
+#include <string.h>
 #include "memory.h"
+#include <stdio.h>
+
+typedef struct
+{
+	int size;
+	int free; // 0 = no block, 1 = free, 2 = unfree
+} MemBlock;
+
+void* start = NULL;
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize)
+{
+	if (start == NULL)
+		start = calloc(1000000, 1);
+
+	// If pointer is NULL, return new block of newSize
+	// Otherwise, find pointer, copy its data to a block of newSize, and free the old block
+
+	MemBlock* oldBlock = pointer != NULL ? (int*)pointer - 1 : NULL;
+	printf("PTR -> Old Block: %p -> %p\n", pointer, oldBlock);
+
+	if (newSize == 0)
+	{
+		if (oldBlock)
+		{
+			oldBlock->free = 1;
+		}
+
+		return;
+	}
+
+	MemBlock* curr = start;
+
+	if (curr->free != 0)
+	{
+		printf("Initial block is not free...\n");
+		while (curr != NULL && (curr->free == 2 || curr->size < newSize))
+		{
+			curr += curr->size;
+			printf("Checking %p. Size: %d. Free: %d", curr - start, curr->size, curr->free);
+		}
+		printf("Found suitable block at %p. Size: %d\n", curr - start, curr->size);
+
+		// Create a new block out of the unneeded space
+		MemBlock* newBlock = curr + newSize / sizeof(MemBlock);
+		newBlock->size = curr->size - newSize - sizeof(MemBlock); // Remember space for metadata!
+		newBlock->free = 1;
+	}
+
+	// Found space
+	curr->free = 2;
+	curr->size = newSize;
+
+	printf("Curr: %p\n", curr - start);
+
+	if (oldBlock)
+	{
+		printf("Reallocating...\n");
+		oldBlock->free = 1;
+		memcpy(pointer, curr + 1, oldSize);
+	}
+
+	return curr;
+}
+
+void* reallocateOld(void* pointer, size_t oldSize, size_t newSize)
 {
 	if (newSize == 0)
 	{
