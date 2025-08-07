e68852b195ba2395314479462f775e80fd5ad2d8
renatodellosso <rddellosso4@gmail.com>
2025-08-07 18:05:21 -0400

    Superclasses and inheritance

--- src/vm.c
+++ src/vm.c
@@ -474,6 +474,17 @@ static InterpretResult run()
 			push(value);
 			break;
 		}
+		case OP_GET_SUPER:
+		{
+			ObjString* name = READ_STRING();
+			ObjClass* superClass = AS_CLASS(pop());
+
+			if (!bindMethod(superClass, name))
+			{
+				return INTERPRET_RUNTIME_ERROR;
+			}
+			break;
+		}
 		case OP_EQUAL:
 		{
 			// Stack, so b first
@@ -565,6 +576,20 @@ static InterpretResult run()
 			frame = &vm.frames[vm.frameCount - 1];
 			break;
 		}
+		case OP_SUPER_INVOKE:
+		{
+			ObjString* method = READ_STRING();
+			int argCount = READ_BYTE();
+			ObjClass* superclass = AS_CLASS(pop());
+
+			if (!invokeFromClass(superclass, method, argCount))
+			{
+				return INTERPRET_RUNTIME_ERROR;
+			}
+
+			frame = &vm.frames[vm.frameCount - 1];
+			break;
+		}
 		case OP_CLOSURE:
 		{
 			ObjFunction* function = AS_FUNCTION(READ_CONSTANT());
@@ -614,6 +639,20 @@ static InterpretResult run()
 		case OP_CLASS:
 			push(OBJ_VAL(newClass(READ_STRING())));
 			break;
+		case OP_INHERIT:
+		{
+			Value superclass = peek(1);
+			if (!IS_CLASS(superclass))
+			{
+				runtimeError("Superclass must be a class.");
+				return INTERPRET_RUNTIME_ERROR;
+			}
+
+			ObjClass* subclass = AS_CLASS(peek(0));
+			tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods); // OP_INHERIT is before all OP_METHODs, so override still work
+			pop(); // Subclass
+			break;
+		}
 		case OP_METHOD:
 			defineMethod(READ_STRING());
 			break;
